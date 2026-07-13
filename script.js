a0da77292b286864a9d4ea79b8ce1fbfbc3d849b
katwallilakatwallila <katwallilakatwalila@gmail.com>
2026-07-13 15:19:44 +0900

    Add cafe website JavaScript features

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function showMessage(){
     alert("Thank you for choosing Malai Cafe! ☕");
-}
+}// Welcome message
+window.onload = function () {
+    alert("Welcome to our Cafe!");
+};
+
+// Menu button interaction
+const menuItems = document.querySelectorAll(".item");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", function () {
+        alert("You selected: " + this.querySelector("h3").innerText);
+    });
+});
+
+// Change header color on scroll
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.style.backgroundColor = "#4b2e1f";
+    } else {
+        header.style.backgroundColor = "#6f4e37";
+    }
+});
